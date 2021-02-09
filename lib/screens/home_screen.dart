08479e2e79c7f68d2f5f9acaf11c086d0a69ac34
TjandraD <t.darmo.t@gmail.com>
2021-02-09 15:50:08 +0700

    Restructured search delegate

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:rest_o/data/model/restaurant.dart';
-import 'package:rest_o/widgets/resto_card.dart';
 import '../widgets/main_resto_list.dart';
+import '../widgets/resto_search.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String id = 'home_screen';
@@ -26,105 +25,6 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
       body: MainRestoList(),
-    );
-  }
-}
-
-class RestaurantSearch extends SearchDelegate<Restaurant> {
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
-      ),
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, null);
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    return FutureBuilder(
-      future: DefaultAssetBundle.of(context)
-          .loadString('assets/data/local_restaurant.json'),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          final List<Restaurant> restaurants = parseRestaurant(snapshot.data)
-              .where((r) => r.name.toLowerCase().contains(query))
-              .toList();
-
-          return ListView.builder(
-            itemCount: restaurants.length,
-            itemBuilder: (context, index) {
-              Restaurant restaurant = restaurants[index];
-              return RestoCard(
-                imgUrl: restaurant.pictureId,
-                name: restaurant.name,
-                city: restaurant.city,
-                rating: restaurant.rating,
-              );
-            },
-          );
-        } else if (snapshot.hasError) {
-          return Center(
-            child: Icon(Icons.error),
-          );
-        } else {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-      },
-    );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    return FutureBuilder(
-      future: DefaultAssetBundle.of(context)
-          .loadString('assets/data/local_restaurant.json'),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          final List<Restaurant> restaurants = parseRestaurant(snapshot.data)
-              .where((r) => r.name.toLowerCase().contains(query))
-              .toList();
-
-          return ListView.builder(
-            itemCount: restaurants.length,
-            itemBuilder: (context, index) {
-              Restaurant restaurant = restaurants[index];
-              return ListTile(
-                title: Text(restaurant.name),
-                onTap: () {
-                  query = restaurant.name.toLowerCase();
-                },
-              );
-            },
-          );
-        } else if (snapshot.hasError) {
-          return Center(
-            child: Column(
-              children: [
-                Icon(Icons.error),
-                Text('Couldn\'t load data!'),
-              ],
-            ),
-          );
-        } else {
-          return Container();
-        }
-      },
     );
   }
 }
---
+++ lib/widgets/resto_search.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:rest_o/data/model/restaurant.dart';
+import 'package:rest_o/widgets/resto_card.dart';
+
+class RestaurantSearch extends SearchDelegate<Restaurant> {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return FutureBuilder(
+      future: DefaultAssetBundle.of(context)
+          .loadString('assets/data/local_restaurant.json'),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final List<Restaurant> restaurants = parseRestaurant(snapshot.data)
+              .where((r) => r.name.toLowerCase().contains(query))
+              .toList();
+
+          return ListView.builder(
+            itemCount: restaurants.length,
+            itemBuilder: (context, index) {
+              Restaurant restaurant = restaurants[index];
+              return RestoCard(
+                imgUrl: restaurant.pictureId,
+                name: restaurant.name,
+                city: restaurant.city,
+                rating: restaurant.rating,
+              );
+            },
+          );
+        } else if (snapshot.hasError) {
+          return Center(
+            child: Icon(Icons.error),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return FutureBuilder(
+      future: DefaultAssetBundle.of(context)
+          .loadString('assets/data/local_restaurant.json'),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          final List<Restaurant> restaurants = parseRestaurant(snapshot.data)
+              .where((r) => r.name.toLowerCase().contains(query))
+              .toList();
+
+          return ListView.builder(
+            itemCount: restaurants.length,
+            itemBuilder: (context, index) {
+              Restaurant restaurant = restaurants[index];
+              return ListTile(
+                title: Text(restaurant.name),
+                onTap: () {
+                  query = restaurant.name.toLowerCase();
+                },
+              );
+            },
+          );
+        } else if (snapshot.hasError) {
+          return Center(
+            child: Column(
+              children: [
+                Icon(Icons.error),
+                Text('Couldn\'t load data!'),
+              ],
+            ),
+          );
+        } else {
+          return Container();
+        }
+      },
+    );
+  }
+}
