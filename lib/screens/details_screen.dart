4a3aa72e299ce3b7fff67f9cf600e362e61faf67
TjandraD <t.darmo.t@gmail.com>
2021-03-09 10:56:40 +0700

    ui: add back button and fix progressindicator position

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -29,173 +29,194 @@ class _DetailsScreenState extends State<DetailsScreen> {
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
     return Scaffold(
       body: SingleChildScrollView(
-        child: FutureBuilder(
-          future: _restaurant,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              final Restaurant restaurant = snapshot.data.restaurant;
+        child: SafeArea(
+          child: FutureBuilder(
+            future: _restaurant,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final Restaurant restaurant = snapshot.data.restaurant;
 
-              return Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(16.0),
+                return Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Row(
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.arrow_back),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ],
                       ),
-                      child: Hero(
-                        tag: 'image_${restaurant.id}',
-                        child: CachedNetworkImage(
-                          imageUrl:
-                              'https://restaurant-api.dicoding.dev/images/medium/${restaurant.pictureId}',
-                          progressIndicatorBuilder:
-                              (context, url, downloadProgress) => Center(
-                                  child: CircularProgressIndicator(
-                                      value: downloadProgress.progress)),
-                          errorWidget: (context, url, error) =>
-                              Icon(Icons.error),
+                      ClipRRect(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(16.0),
+                        ),
+                        child: Hero(
+                          tag: 'image_${restaurant.id}',
+                          child: CachedNetworkImage(
+                            imageUrl:
+                                'https://restaurant-api.dicoding.dev/images/medium/${restaurant.pictureId}',
+                            progressIndicatorBuilder:
+                                (context, url, downloadProgress) => Center(
+                                    child: CircularProgressIndicator(
+                                        value: downloadProgress.progress)),
+                            errorWidget: (context, url, error) =>
+                                Icon(Icons.error),
+                          ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(
-                        vertical: 16.0,
-                        horizontal: 10.0,
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 16.0,
+                          horizontal: 10.0,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  restaurant.name,
+                                  style: Theme.of(context).textTheme.headline5,
+                                ),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.location_on,
+                                      size: 24.0,
+                                    ),
+                                    SizedBox(
+                                      width: 2.0,
+                                    ),
+                                    Text(
+                                      restaurant.city,
+                                      style:
+                                          Theme.of(context).textTheme.headline6,
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 4.0,
+                                ),
+                                SmoothStarRating(
+                                  allowHalfRating: true,
+                                  starCount: 5,
+                                  rating: restaurant.rating,
+                                  size: 24,
+                                  isReadOnly: true,
+                                  color: Theme.of(context).primaryColor,
+                                  borderColor: Theme.of(context).primaryColor,
+                                ),
+                              ],
+                            ),
+                            IconButton(
+                              icon: Icon(isRestoFavorited
+                                  ? Icons.favorite
+                                  : Icons.favorite_outline),
+                              color: isRestoFavorited
+                                  ? Colors.red
+                                  : Colors.grey[600],
+                              onPressed: () {
+                                setState(() {
+                                  isRestoFavorited = !isRestoFavorited;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      Container(
+                        child: Text(
+                          restaurant.description,
+                          style: Theme.of(context).textTheme.bodyText1,
+                          textAlign: TextAlign.justify,
+                        ),
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 16.0,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 16.0,
+                      ),
+                      Row(
                         children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                restaurant.name,
-                                style: Theme.of(context).textTheme.headline5,
-                              ),
-                              Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.location_on,
-                                    size: 24.0,
-                                  ),
-                                  SizedBox(
-                                    width: 2.0,
-                                  ),
-                                  Text(
-                                    restaurant.city,
-                                    style:
-                                        Theme.of(context).textTheme.headline6,
-                                  ),
-                                ],
-                              ),
-                              SizedBox(
-                                height: 4.0,
-                              ),
-                              SmoothStarRating(
-                                allowHalfRating: true,
-                                starCount: 5,
-                                rating: restaurant.rating,
-                                size: 24,
-                                isReadOnly: true,
-                                color: Theme.of(context).primaryColor,
-                                borderColor: Theme.of(context).primaryColor,
-                              ),
-                            ],
+                          SizedBox(
+                            width: 16.0,
                           ),
-                          IconButton(
-                            icon: Icon(isRestoFavorited
-                                ? Icons.favorite
-                                : Icons.favorite_outline),
-                            color: isRestoFavorited
-                                ? Colors.red
-                                : Colors.grey[600],
-                            onPressed: () {
+                          FnBSelector(
+                            title: 'Food',
+                            menuSelected: menuSelected,
+                            onTap: () {
                               setState(() {
-                                isRestoFavorited = !isRestoFavorited;
+                                if (menuSelected == 'Beverage') {
+                                  menuSelected = 'Food';
+                                }
+                              });
+                            },
+                          ),
+                          SizedBox(
+                            width: 8.0,
+                          ),
+                          FnBSelector(
+                            title: 'Beverage',
+                            menuSelected: menuSelected,
+                            onTap: () {
+                              setState(() {
+                                if (menuSelected == 'Food') {
+                                  menuSelected = 'Beverage';
+                                }
                               });
                             },
                           ),
                         ],
                       ),
-                    ),
-                    Container(
-                      child: Text(
-                        restaurant.description,
-                        style: Theme.of(context).textTheme.bodyText1,
-                        textAlign: TextAlign.justify,
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 8.0,
+                        ),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 8.0,
+                        ),
+                        width: double.infinity,
+                        height: 150.0,
+                        child: MenuList(
+                          menuSelected: menuSelected,
+                          foods: restaurant.menus.foods,
+                          drinks: restaurant.menus.drinks,
+                        ),
                       ),
-                      margin: EdgeInsets.symmetric(
-                        horizontal: 16.0,
-                      ),
-                    ),
+                    ],
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Icon(Icons.error),
+                );
+              } else {
+                return Column(
+                  children: [
                     SizedBox(
-                      height: 16.0,
+                      height: (height / 2) - 50,
+                      width: width,
                     ),
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: 16.0,
-                        ),
-                        FnBSelector(
-                          title: 'Food',
-                          menuSelected: menuSelected,
-                          onTap: () {
-                            setState(() {
-                              if (menuSelected == 'Beverage') {
-                                menuSelected = 'Food';
-                              }
-                            });
-                          },
-                        ),
-                        SizedBox(
-                          width: 8.0,
-                        ),
-                        FnBSelector(
-                          title: 'Beverage',
-                          menuSelected: menuSelected,
-                          onTap: () {
-                            setState(() {
-                              if (menuSelected == 'Food') {
-                                menuSelected = 'Beverage';
-                              }
-                            });
-                          },
-                        ),
-                      ],
-                    ),
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 8.0,
-                      ),
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 8.0,
-                      ),
-                      width: double.infinity,
-                      height: 150.0,
-                      child: MenuList(
-                        menuSelected: menuSelected,
-                        foods: restaurant.menus.foods,
-                        drinks: restaurant.menus.drinks,
-                      ),
-                    ),
+                    CircularProgressIndicator(),
                   ],
-                ),
-              );
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Icon(Icons.error),
-              );
-            } else {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
+                );
+              }
+            },
+          ),
         ),
       ),
     );
